e4cef1c3dd9a66d41f56db8585366d52db35f1c7
Roma8642 <froma5361@gmail.com>
2023-11-15 23:38:15 +0200

    addCardsEl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './style/index.scss';
-import Card from "./components/card";
+import Card from "./components/Card";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Drawer from "./components/Drawer";
+const crossList=[{
+    title:'Мужские Кроссовки Nike Blazer Mid Suede',
+    price:12999,
+    imgUrl:'/img/sneakers/image 1.jpg'
+},{
+    title:'Мужские Кроссовки Nike Air Max 270',
+    price:12999,
+    imgUrl:'/img/sneakers/image 2.jpg'
+},{
+    title:'Мужские Кроссовки Nike Blazer Mid Suede',
+    price:8499,
+    imgUrl:'/img/sneakers/image 3.jpg'
+},{
+    title:'Мужские Кроссовки Nike Blazer Mid Suede',
+    price:8499,
+    imgUrl:'/img/sneakers/image 3.jpg'
+},
+]
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,7 +35,9 @@ function App() {
         </div>
 
 <div className="d-flex">
-  <Card title={'Мужские Кроссовки Nike Blazer Mid Suede'} price={'12 999'}/>
+    {crossList.map((obj)=>(
+        <Card title={obj.title} price={obj.price} imgUrl={obj.imgUrl}/>
+    ))}
 </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Card = ({title,price}) => {
+const Card = (props) => {
     return (
         <div className="card">
             <div className="favorite">
                 <img src="/img/unlike.svg" alt="unliked"/>
             </div>
-            <img width={133} height={112} src="/img/sneakers/image%201.jpg" alt="sneakers"/>
-            <h5>{title}</h5>
+            <img width={133} height={112} src={props.imgUrl} alt="sneakers"/>
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена</span>
-                    <b>{price}</b>
+                    <b>{props.price}</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/img/plusBtn.svg" alt="sneakers"/>
